657e6e4346e48312cb47013256efdfbc6db87c39
Kimberly Keller <kimberly@gravitaspublications.com>
2015-10-22 11:43:54 -0600

    added audience and goal text

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -12,7 +12,16 @@
 		This website will serve as the host for all the relevant information about me, my certifications, abilities, and philosophies.
 		It will also house testimonials from previous clients and recommendations to other health and wellness practitioners that I trust.
 		</p>
-		<p><strong>Audience</strong></p>
-		<p><strong>Goal</strong></p>
+		<p><strong>Audience:</strong> Armature athletes and casual gym goers looking to improve their performance through mobility and strength training,
+		individuals looking to improve their life by focusing and/or starting on their physical health,
+		individuals looking to reduce pain through healthy movement patterns, individuals looking for a fitness workaround
+		for a recent or chronic injury.
+		</p>
+		<p><strong>Goal:</strong> Create an aesthetically pleasing, organized website that serves at the central hub of Lotus Cat activity.
+		Clients should have a strong draw to the design of the website as well as a quick, intuitive understanding of how to find
+		the information they are looking for. The big picture goal of Lotus Cat is to fill a gap in the health and wellness community
+		by providing strength training with an emphasis on functional, healthy, mobile, tissue, excellent breath quality, and good communication
+		with the body.
+		</p>
 	</body>
 </html>
