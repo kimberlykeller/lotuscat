74cc32278e1e6e961adf07a8189a3ac4697b564c
Kimberly Keller <kimberly@gravitaspublications.com>
2015-10-22 11:52:29 -0600

    added basic html structure for milestone 2

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Milestone 2 For Lotus Cat</title>
+	</head>
+	<body>
+		<h1>Milestone 2 For Lotus Cat</h1>
+		<p></p>
+	</body>
+</html>
