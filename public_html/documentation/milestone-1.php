c5d19ec2087cd9920c16860358db37e58fda443a
Kimberly Keller <kimberly@gravitaspublications.com>
2015-10-22 11:20:56 -0600

    added milestone 1 documentation and purpose

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Milestone 1 Documentation</title>
+	</head>
+	<body>
+		<h1>Milestone 1 for Lotus Cat</h1>
+		<p><strong>Purpose:</strong> Lotus Cat Movement is a personal training and wellness resource for people who are interested in
+		better living through healthy movement. This website is the place where new and returning clients can find out information
+		about services, location, expectations, etc. as well as schedule/reschedule appointments and contact me through an HTML 5 webform.
+		This website will serve as the host for all the relevant information about me, my certifications, abilities, and philosophies.
+		It will also house testimonials from previous clients and recommendations to other health and wellness practitioners that I trust.
+		</p>
+		<p><strong>Audience</strong></p>
+		<p><strong>Goal</strong></p>
+	</body>
+</html>
