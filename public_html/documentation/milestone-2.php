3395a14f57fab7d6f333b9f5664ed1fb4d43fdd2
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-11 21:36:37 -0700

    added persona, use case, and site map

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,6 +6,47 @@
 	</head>
 	<body>
 		<h1>Milestone 2 For Lotus Cat</h1>
-		<p></p>
+		<h3>Introduction</h3>
+		<p>The following document outlines the user experience and interaction design for the Lotus Cat Movement website
+		project. This project is the professional web presence for Lotus Cat Movement. It houses all the necessary information for a
+		potential new client to research and contact me, including qualifications, driving philosophies, services offered, and contact information.
+		The site is designed for a quality oriented, discerning, professional client who is primarily on the go and mobile driven. The primary
+		goal of the website is to initiate contact between myself and potential new clients. </p>
+		<h3>Persona</h3>
+		<p><strong>Laura</strong> is in her early 50s and she has recently taken up regular cycling. Laura has always
+		been an active person but she wants to build strength and mobility to accentuate her new sport. She is interested
+		in individualized help and will want one on one sessions or small groups geared toward her needs. Laura is upper middle
+		class and has disposable income that she likes to spend on her hobbies, cycling equiptment, yoga classes, etc. Laura is also health
+		conscious and takes a holistic approach to fitness and wellness. She wants to make sure
+		that the trainers and coaches she works with are invested in her well being as well as her athletic performance. Her goal is to quickly
+		browse Lotus Cat and get a sense of my driving philosophies and qualifications, once comfortable, she wants to quickly submit a contact
+		request to set up an appointment. She will primarly accesss the website through her phone or tablet. She has one of the most recent generations of devices and upgrades regularly.
+		<h3>Use Case</h3>
+		<p>Laura is chatting with her cycling partner and friend one chilly October morning. She's interested in improving her cycling time in the upcoming
+		meet. She's tried everything, but between her work life, home life, and social life, she doesn't have time to be her own trainer. Her friend
+		Rachel tells her to check out Lotus Cat, there are one on one sessions, small classes, and group rates. Adding some strength and mobility work to
+		her routine really helped Rachel's cycling performance.
+		</p>
+		<p>
+		Laura parts ways with Rachel and quickly looks up "Lotus Cat" on google on her mobile device before getting ready to start her busy weekend. She pulls up a
+		clean home page that quickly gives her a sense of professionalism and training knowledge. She clicks on a "Services" link and sees that there are one on
+		one strength training sessions and weekend yoga classes in the park. Laura then clicks the "About" page and sees that  Kimberly specializes in movement patterns and yoga,
+		and is certified through multiple training organizations. Laura sees a "Schedule An Appointment" button at the bottom of the page and clicks on it.
+		She is redirected to a contact form where she quickly puts in her name, email, and phone number. Laura is excited to hear back and happy with how easy
+		it was to access the site and contact me. Shortly after she is contacted by me and we arrange a meeting
+		to discuss her goals.
+		<h3>Proposed Site Map</h3>
+		<ul>
+			<li>Home</li>
+			<li>Services</li>
+			<li>About</li>
+			<li>Contact</li>
+		</ul>
+		<p><strong>Home</strong> A brief introduction to the site with a clear purpose/mission and a call to action</p>
+		<p><strong>Services</strong> A brief explanation of each service offered as well as a very brief overview of my personal training philosophies.
+		Also includes a button redirects them to the contact page.</p>
+		<p><strong>About</strong> My certs, philosophies, and a brief personal history</p>
+		<p><strong>Contact</strong> A contact form so they may get in touch with me</p>
+
 	</body>
 </html>
