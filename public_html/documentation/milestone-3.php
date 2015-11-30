1b1219a94034ab81d8f1de10e07b8768f3ac8c98
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-29 19:58:29 -0700

    added milestone 3

---
+++ public_html/documentation/milestone-3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Milestone 3 Documentation</title>
+	</head>
+	<body>
+		<h1>Milestone 3 Documentation</h1>
+		<p>This layout is a very clean, lightweight Z pattern design. Meant to give a concise description of the 
+		purpose of the site and direct users to the navigation and contact forms. The color scheme will be greens and neutrals
+		no more than 2 main colors. There will be a decorative typeface used only for the lotus cat logo area. The content area will
+		feature a high quality photo and the body text. </p>
+		
+		<h3>Front Page Desktop</h3>
+		<img src="/public_html/img/frontpage-desktop.jpeg">
+		<h3>Content View Desktop</h3>
+		<img src="/public_html/img/contentview-desktop.jpeg">
+		<h3>Mobile Views</h3>
+		<img src="/public_html/img/mobile.jpeg">
+	</body>
