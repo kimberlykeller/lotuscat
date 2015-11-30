24eddd1af1433533cbf34d09961d99af5546f827
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-29 20:14:37 -0700

    added features list

--- public_html/documentation/milestone-3.php
+++ public_html/documentation/milestone-3.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Milestone 3 Documentation</title>
+		<link href="../css/custom.css" rel="stylesheet">
 	</head>
 	<body>
 		<h1>Milestone 3 Documentation</h1>
@@ -10,12 +11,18 @@
 		purpose of the site and direct users to the navigation and contact forms. The color scheme will be greens and neutrals
 		no more than 2 main colors. There will be a decorative typeface used only for the lotus cat logo area. The content area will
 		feature a high quality photo and the body text. </p>
+		<h3>Features</h3>
+		<ul>
+			<li>Modal for contact form</li>
+			<li>Responsive</li>
+			<li>Contact buttons for extra nudge to contact</li>
+		</ul>
 		
 		<h3>Front Page Desktop</h3>
-		<img src="../img/frontpage-desktop.jpeg">
+		<img class="img-sm" src="../img/frontpage-desktop.jpeg">
 		<h3>Content View Desktop</h3>
-		<img src="../img/contentview-desktop.jpeg">
+		<img class="img-sm" src="../img/contentview-desktop.jpeg">
 		<h3>Mobile Views</h3>
-		<img src="../img/mobile.jpeg">
+		<img class="img-sm" src="../img/mobile.jpeg">
 	</body>
 </html>
